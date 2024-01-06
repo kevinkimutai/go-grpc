2c40676d5393d38b2d6a25623530520f3f41cf42
unknown <kevinkimutai62@gmail.com>
2024-01-06 22:05:17 +0300

    protobuf files

--- order/adapters/grpc/server.go
+++ order/adapters/grpc/server.go
@@ -2,9 +2,11 @@ package grpc
 
 import (
 	"e-commerce-order/ports"
+	"e-commerce-order/proto/golang/order"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -20,17 +22,17 @@ func NewAdapter(api ports.APIPort, port int) *Adapter {
 }
 
 func (a Adapter) Run() {
- var err error
- listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
- if err != nil {
- log.Fatalf("failed to listen on port %d, error: %v", a.port, err)
- }
- grpcServer := grpc.NewServer()
- order.RegisterOrderServer(grpcServer, a)
- if config.GetEnv() == "development" {
- reflection.Register(grpcServer)
- }
- if err := grpcServer.Serve(listen); err != nil {
- log.Fatalf("failed to serve grpc on port ")
- }
+	var err error
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		log.Fatalf("failed to listen on port %d, error: %v", a.port, err)
+	}
+	grpcServer := grpc.NewServer()
+	order.RegisterOrderServer(grpcServer, a)
+	if os.Getenv("ENV") == "development" {
+		reflection.Register(grpcServer)
+	}
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve grpc on port ")
+	}
 }
--- order/go.mod
+++ order/go.mod
@@ -15,4 +15,6 @@ require (
 	google.golang.org/protobuf v1.31.0 // indirect
 	gorm.io/driver/mysql v1.5.2 // indirect
 	gorm.io/gorm v1.25.5 // indirect
+	
+
 )
