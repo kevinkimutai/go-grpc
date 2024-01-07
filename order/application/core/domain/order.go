2f5d380e55451e7d697bc94d1e3ffa166f347aba
unknown <kevinkimutai62@gmail.com>
2024-01-07 23:29:49 +0300

    total price def

--- order/application/core/domain/order.go
+++ order/application/core/domain/order.go
@@ -15,3 +15,11 @@ type Order struct {
 	OrderItems []OrderItem `json:"order_items"`
 	CreatedAt  time.Time   `json:"created_at"`
 }
+
+func (o *Order) TotalPrice() float32 {
+	var totalPrice float32
+	for _, orderItem := range o.OrderItems {
+		totalPrice += orderItem.UnitPrice * float32(orderItem.Quantity)
+	}
+	return totalPrice
+}
